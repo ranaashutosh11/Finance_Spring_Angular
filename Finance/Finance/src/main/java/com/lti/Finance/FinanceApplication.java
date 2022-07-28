a4770b18435e4d4165358c08d746a3ac2d944a63
abssinghal <abssinghal@gmail.com>
2022-07-28 14:41:52 +0530

    Commit 3

--- Finance/Finance/src/main/java/com/lti/Finance/FinanceApplication.java
+++ Finance/Finance/src/main/java/com/lti/Finance/FinanceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FinanceApplication {
 	
-	//Added a comment
+	//Added a comment comment
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceApplication.class, args);
